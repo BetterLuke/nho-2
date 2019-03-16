da2baf264a32519d17b3d892fbeeeaca8144d59b
weenta <weenta@qq.com>
2019-03-16 16:44:58 +0800

    home-header

---
+++ src/components/home-header/HomeHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeHeader = () => {
+  return (
+    <div className='home-header'>
+      <input className='home-input'/>
+      <button>搜索</button>
+    </div>
+  )
+}
+
+export default HomeHeader
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './home.scss';
 
+import HomeHeader from '../../components/home-header/HomeHeader'
+
+
 const Home = () => (
   <div>
     <h2>home page</h2>
+    <HomeHeader />
   </div>
 )
 
